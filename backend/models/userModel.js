41438261de1755debd7809b69735786de088b0fa
sravyaravulakolla <ravulakollasravya@gmail.com>
2024-08-26 23:16:08 +0530

    Updated frontend and added schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,11 @@
+const mongoose= require("mongoose");
+const userSchema= mongoose.Schema({
+    name:{type:String, required:true},
+    email:{type:String, required:true},
+    password:{type:String, required: true},
+    pic:{type: String, required: true, default:"https://www.google.com/imgres?q=default%20avatar%20profile%20icon&imgurl=https%3A%2F%2Ft4.ftcdn.net%2Fjpg%2F05%2F49%2F98%2F39%2F360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg&imgrefurl=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Ddefault%2Bavatar&docid=poxYYtr45wzCGM&tbnid=hy8oTwjvtmAQ7M&vet=12ahUKEwiOkt3n4ZKIAxX_xTgGHZQ2LhkQM3oECFUQAA..i&w=360&h=360&hcb=2&ved=2ahUKEwiOkt3n4ZKIAxX_xTgGHZQ2LhkQM3oECFUQAA"},    
+},
+{timestamps:true},
+);
+const User= mongoose.model("User", userSchema);
+module.exports=User;
