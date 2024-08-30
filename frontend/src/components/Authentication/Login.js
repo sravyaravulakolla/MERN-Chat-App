cf45582b448a866587cdd34b291ba669b109965c
sravyaravulakolla <ravulakollasravya@gmail.com>
2024-08-30 22:02:51 +0530

    Added login and few functions of one-on-one chat

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,29 +1,66 @@
 import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
-
+import axios from 'axios';
+import { useToast } from '@chakra-ui/react';
+import { useHistory } from 'react-router-dom';
 const Login = () => {
     const [show, setShow]= useState(false);
-    const [name, setName]=useState();
     const [email, setEmail]=useState();
     const [password, setPassword]=useState();
+    const [loading, setLoading]= useState(false);
+    const toast= useToast();
+    const history= useHistory();
+    
     const handleClick=()=>{
         setShow(!show);
     };
-    const postDetails=(pic)=>{
-
+    const submitHandler= async()=>{
+        setLoading(true);
+        if(!email || !password){
+          toast({
+          title: 'Please fill all the Fields',
+          status: 'warning',
+          duration: 5000,
+          isClosable: true,
+          position:"bottom",
+          });
+          setLoading(false);
+          return;
+        }
+        try{
+          const config={
+            headers:{
+              "Content-type":"application/json",
+            },
+          };
+          const {data}= axios.post("/api/user/login",{email, password}, config);
+          // console.log("Login successful");
+          
+          toast({
+          title: 'Login Successful',
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+          position:"bottom",
+          });
+          localStorage.setItem("userInfo", JSON.stringify(data));
+          setLoading(false);
+          history.push("/chats")
+        }
+        catch(error){
+          toast({
+          title: 'Error Occured!',
+          description: error.response.data.message,
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+          position:"bottom",
+          });
+          setLoading(false);
+        }
     };
-    const submitHandler=()=>{
-        
-    }
   return (
     <VStack spacing={"5px"}>
-      <FormControl id="first-name" isRequired>
-        <FormLabel>
-            Name
-        </FormLabel>
-        <Input placeholder='Enter your Name'
-        onChange={(e)=>setName(e.target.value)}/>
-      </FormControl>
       <FormControl id="email" isRequired>
         <FormLabel>
             Email
@@ -44,7 +81,7 @@ const Login = () => {
             </InputRightElement>
         </InputGroup>
       </FormControl>
-      <Button colorScheme='blue' width={"100%"} style={{marginTop: 15}} onClick={submitHandler}>
+      <Button colorScheme='blue' width={"100%"} style={{marginTop: 15}} onClick={submitHandler} isLoading={loading}>
         Login
       </Button>
     </VStack>
